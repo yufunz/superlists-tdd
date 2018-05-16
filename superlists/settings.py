35d1d559349136f3394d3d73d97baf916c7d4a65
ThePandaOne <locolocopoko@hotmail.com>
2018-05-16 16:03:11 +1200

    Removed keys from settings file; replaced with env vars

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6k9t3k560%ambi+q4vzh4844)q!@5@&l1=)pnb0x=ok0-2+(%1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
